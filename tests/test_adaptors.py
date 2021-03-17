d698c7b0a0fe5ad4a679cf54d010329e4dcfbbb9
José Sánchez-Gallego <gallegoj@uw.edu>
2021-03-17 12:54:19 -0700

    Additional tests for adaptors

--- tests/test_adaptors.py
+++ tests/test_adaptors.py
@@ -6,6 +6,8 @@
 # @Filename: test_adaptors.py
 # @License: BSD 3-clause (http://www.opensource.org/licenses/BSD-3-Clause)
 
+import pytest
+
 from drift import adaptors
 
 
@@ -25,3 +27,17 @@ def test_rtd():
 
     assert adaptors.rtd(1) == (0.1, "degC")
     assert adaptors.rtd(8510)[0] == -5702.5
+
+
+def test_rtd10():
+
+    assert adaptors.rtd10(101) == (10.1, "degC")
+
+
+@pytest.mark.parametrize(
+    "raw,v_min,v_max,expected",
+    [(0, 0, 30, 0), (32760, 0, 30, 30)],
+)
+def test_voltage(raw, v_min, v_max, expected):
+
+    assert adaptors.voltage(raw, v_min, v_max) == (expected, "V")
