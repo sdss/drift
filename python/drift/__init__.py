53aa3b60e4a8f2ec0f864bbae7370dc8c0fe5b24
José Sánchez-Gallego <gallegoj@uw.edu>
2021-03-23 17:12:40 -0700

    Add some warning ignores

--- python/drift/__init__.py
+++ python/drift/__init__.py
@@ -1,10 +1,16 @@
 # encoding: utf-8
 
+import warnings
+
 from sdsstools import get_logger, get_package_version
 
 
 # pip package name
 NAME = "sdss-drift"
+
+
+warnings.filterwarnings("ignore", message=".+decorator is deprecated since Python.+")
+warnings.filterwarnings("ignore", message=".+The loop argument is deprecated.+")
 
 
 log = get_logger(NAME)
